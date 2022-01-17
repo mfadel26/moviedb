988e87c38ac4818fbfb28a267858a518a7232d43
mfadel26 <muhammadfadelp@gmail.com>
2022-01-17 13:29:23 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -2,49 +2,53 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, useRouteMatch } from "react-router-dom";
 import Movie from "./pages/Home";
 import MovieId from "./pages/MovieId";
-import {
-	Layout,
-	Menu,
-	Image
-} from "antd";
+import { Layout, Menu, Image } from "antd";
 import "./App.css";
+import FontAwesome from "react-fontawesome";
 const { Header, Footer } = Layout;
 export default function App() {
 	return (
 		<Router>
-				<Header >
-					<Menu
-						theme="dark"
-						mode="horizontal"
-						defaultSelectedKeys={["1"]}
-						style={{ float: "right" }}>
-						<Menu.Item>
-							<a href="https://github.com/mfadel26/moviedb">Github - M Fadel</a>{" "}
-						</Menu.Item>
-						<Menu.Item key="2">
-							<Image
-								preview={false}
-								width={50}
-								src={
-									"https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
-								}
-							/>
-						</Menu.Item>
-					</Menu>
-				</Header>
-				<div className="App" >
-					<Switch>
-						<Route path="/Movie">
-							<Detail />
-						</Route>
-						<Route path="/">
-							<Movie />
-						</Route>
-					</Switch>
-          </div>
-				<Footer style={{ textAlign: "center" }}>
-					Ant Design ©2018 Created by Ant UED
-				</Footer>      
+			<Header>
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					defaultSelectedKeys={["1"]}
+					style={{ float: "right" }}>
+					<Menu.Item>
+						<a href="https://github.com/mfadel26/moviedb">Github - M Fadel</a>{" "}
+					</Menu.Item>
+					<Menu.Item key="2">
+						<Image
+							preview={false}
+							width={50}
+							src={
+								"https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+							}
+						/>
+					</Menu.Item>
+				</Menu>
+			</Header>
+			<div className="App">
+				<Switch>
+					<Route path="/Movie">
+						<Detail />
+					</Route>
+					<Route path="/">
+						<Movie />
+					</Route>
+				</Switch>
+			</div>
+			<Footer style={{ textAlign: "center" }}>
+				<p>
+					Made by <a href="https://github.com/mfadel26">mfadel26</a>
+				</p>
+				<p>
+					<a href="https://github.com/mfadel26/moviedb">
+						<FontAwesome name="github" /> View code
+					</a>
+				</p>
+			</Footer>
 		</Router>
 	);
 }
